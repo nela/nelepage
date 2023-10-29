166cb6ac6ba61ecd1171c2f38496d20b6ff37df4
Nemanja Lakicevic <n@lakicevic.no>
2023-10-29 19:43:22 +0100

    feat: add more cubes

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,49 @@
 <script lang="ts">
   import Cube from "./lib/Cube.svelte";
-  const github = { alt: 'github-icon', path: '../public/github-mark.svg' }
+
+  const logoMetadata: Record<
+    string,
+    { alt: string; path: string; background: string }
+  > = {
+    github: {
+      alt: "github-icon",
+      path: "../public/github-mark.svg",
+      background: "linear-gradient(#4f4f4f, #333);",
+    },
+    linked: {
+      alt: "linked-in-icon",
+      path: "../public/linkedin.svg",
+      background: "linear-gradient(#00a0dc, #0077b5);",
+    },
+
+    soundcloud: {
+      alt: "soundcloud-icon",
+      path: "../public/soundcloud.svg",
+      background: "linear-gradient(#faa21b, #f15623);",
+    },
+    uio: {
+      alt: "uio-icon",
+      path: "../public/uio.svg",
+      background: "linear-gradient(#fb6666, #dd0000)",
+    },
+  };
 </script>
 
 <main>
-Nelescope
+  Nelescope
 
-<Cube img={github}/>
-
+  <div class="cubes">
+    <Cube logoMetadata={logoMetadata['github']} />
+    <Cube logoMetadata={logoMetadata['linked']} />
+    <Cube logoMetadata={logoMetadata['soundcloud']} />
+    <Cube logoMetadata={logoMetadata['uio']} />
+  </div>
 </main>
+
+<style lang="scss">
+  .cubes {
+    display: flex;
+    align-items: center;
+    justify-items: center;
+  }
+</style>
